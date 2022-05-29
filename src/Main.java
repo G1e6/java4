7424248caa467425a3e38e93ab7fc31f6c6c137f
G1e6 <gleb-potapov@rambler.ru>
2022-05-29 20:16:23 +0500

    Modernization

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10000;
-        int miles = service.calculate(price);
+        int miles = service.calculate(1000, 20);
         System.out.println(miles);
     }
 }
