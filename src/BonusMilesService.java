9e196702c92746ff2cb24c80ba8c57eccbd6f2e9
G1e6 <gleb-potapov@rambler.ru>
2022-05-29 09:37:51 +0500

    modernization

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,8 +2,8 @@ public class BonusMilesService {
 
     public int calculate(int cost) {
         cost = 20;
-        int price=10000;
-        int miles = price/cost;
+        int price = 10000;
+        int miles = price / cost;
         return miles;
     }
 }
